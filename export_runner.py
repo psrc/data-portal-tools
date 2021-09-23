9a3dccc6c275ea30b4e78efca70c3cff9d6e2e72
Chris Peak <CPeak@psrc.org>
2021-09-23 16:04:40 -0700

    add YAML files for macroeconomic and park_and_ride tabular datasets

--- export_runner.py
+++ export_runner.py
@@ -66,7 +66,7 @@ def export(config):
 run_files = os.listdir('./Config/run_files/')
 for f in run_files:
 #for f in ['cities_test.yml']:
-	if f >= 'tracts2010.yml':
+	if f  in ['park_and_ride.yml']:
 		f_path = './Config/run_files/' + f
 		with open(f_path) as file:
 			config = yaml.load(file, Loader=yaml.FullLoader)
