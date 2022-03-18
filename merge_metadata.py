d349389db9c3dd9a45c9e412ddeb7c96d1303cb0
Chris Peak <CPeak@psrc.org>
2022-03-18 13:49:41 -0700

    merged data stewards metadata form with yaml files

--- merge_metadata.py
+++ merge_metadata.py
@@ -1,4 +1,4 @@
 from form_results import FormResults
 
-f_res = FormResults('Initial PSRC Metadata Collection Form(1-38).xlsx')
+f_res = FormResults('Initial PSRC Metadata Collection Form(31,34,35,36).xlsx')
 f_res.integrate_fields()
