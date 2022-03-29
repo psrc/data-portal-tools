9b85db462f7d057f3c0249c87a9bbbd75f713b2d
Chris Peak <CPeak@psrc.org>
2022-03-29 15:05:45 -0700

    re-export all layers with recently-revised metadata configuration

--- merge_metadata.py
+++ merge_metadata.py
@@ -1,4 +1,4 @@
 from form_results import FormResults
 
-f_res = FormResults('Initial PSRC Metadata Collection Form(31,34,35,36).xlsx')
+f_res = FormResults('Initial PSRC Metadata Collection Form(45-46).xlsx')
 f_res.integrate_fields()
