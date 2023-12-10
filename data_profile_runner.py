6c3705d6e2a7875cc52e2db16d8da713eee7de40
Chris Peak <cpeak@psrc.org>
2023-12-10 09:31:01 -0800

    create data profiles for 2022 ACS5

--- data_profile_runner.py
+++ data_profile_runner.py
@@ -16,16 +16,16 @@ import yaml
 
 datasets =  [
         {'census_year': 2022,
-        'census_product': 'acs1',
+        'census_product': 'acs5',
         'specifications': {
             "DP02": {'short_title': 'Social Characteristics Data Profle', 
-                    'long_title':'ACS Data Profile: Social Characteristics, ACS 2022 1-Year Data'},
+                    'long_title':'ACS Data Profile: Social Characteristics, ACS 2022 5-Year Data'},
             "DP03": {'short_title': 'Economic Data Profle', 
-                    'long_title':'ACS Data Profile: Economic Characteristics, ACS 2022 1-Year Data' },
+                    'long_title':'ACS Data Profile: Economic Characteristics, ACS 2022 5-Year Data' },
             "DP04": {'short_title': 'Housing Data Profle', 
-                    'long_title':'ACS Data Profile: Housing Characteristics, ACS 2022 1-Year Data' },
+                    'long_title':'ACS Data Profile: Housing Characteristics, ACS 2022 5-Year Data' },
             "DP05": {'short_title': 'Demographic Data Profle', 
-                    'long_title':'ACS Data Profile: Demographic Characteristics, ACS 2022 1-Year Data' }    
+                    'long_title':'ACS Data Profile: Demographic Characteristics, ACS 2022 5-Year Data' }    
                 }
         }
 ]
